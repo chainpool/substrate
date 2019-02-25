0fb553027759c685ea213fc0ca8fbcfb88fc7ffa
Aton <jincxmain@vip.163.com>
2019-02-25 15:24:18 +0800

    chainx modify
    support for msgbus-redis/cache-lru
    
    move rpc to node
    
    Make NextKeyFor public
    
    Treat --key as private key of pkcs8
    
    session change block can't package transaction
    
    promote log output
    
    provide millisecond
    provide level in info mode

--- core/service/src/lib.rs
+++ core/service/src/lib.rs
@@ -25,8 +25,8 @@ mod chain_spec;
 pub mod config;
 pub mod chain_ops;
 
-use std::io;
-use std::net::SocketAddr;
+//use std::io;
+//use std::net::SocketAddr;
 use std::collections::HashMap;
 use futures::sync::mpsc;
 use parking_lot::Mutex;
@@ -60,7 +60,7 @@ pub use components::{ServiceFactory, FullBackend, FullExecutor, LightBackend,
 	FactoryFullConfiguration, RuntimeGenesis, FactoryGenesis,
 	ComponentExHash, ComponentExtrinsic, FactoryExtrinsic
 };
-use components::{StartRPC, MaintainTransactionPool, OffchainWorker};
+use components::{/*StartRPC,*/ MaintainTransactionPool, OffchainWorker};
 #[doc(hidden)]
 pub use std::{ops::Deref, result::Result, sync::Arc};
 #[doc(hidden)]
@@ -72,16 +72,16 @@ const DEFAULT_PROTOCOL_ID: &str = "sup";
 
 /// Substrate service.
 pub struct Service<Components: components::Components> {
-	client: Arc<ComponentClient<Components>>,
-	network: Option<Arc<components::NetworkService<Components::Factory>>>,
-	transaction_pool: Arc<TransactionPool<Components::TransactionPoolApi>>,
+	pub client: Arc<ComponentClient<Components>>,
+	pub network: Option<Arc<components::NetworkService<Components::Factory>>>,
+	pub transaction_pool: Arc<TransactionPool<Components::TransactionPoolApi>>,
 	inherents_pool: Arc<InherentsPool<ComponentExtrinsic<Components>>>,
 	keystore: Keystore,
 	exit: ::exit_future::Exit,
 	signal: Option<Signal>,
 	/// Configuration of this Service
 	pub config: FactoryFullConfiguration<Components::Factory>,
-	_rpc: Box<::std::any::Any + Send + Sync>,
+	//_rpc: Box<::std::any::Any + Send + Sync>,
 	_telemetry: Option<Arc<tel::Telemetry>>,
 	_offchain_workers: Option<Arc<offchain::OffchainWorkers<ComponentClient<Components>, ComponentBlock<Components>>>>,
 	_telemetry_on_connect_sinks: Arc<Mutex<Vec<mpsc::UnboundedSender<()>>>>,
@@ -189,7 +189,7 @@ impl<Components: components::Components> Service<Components> {
 			network::ProtocolId::from(protocol_id_full)
 		};
 
-		let has_bootnodes = !network_params.network_config.boot_nodes.is_empty();
+		//let has_bootnodes = !network_params.network_config.boot_nodes.is_empty();
 		let (network, network_chan) = network::Service::new(
 			network_params,
 			protocol_id,
@@ -309,6 +309,7 @@ impl<Components: components::Components> Service<Components> {
 		}
 
 
+/*
 		// RPC
 		let system_info = rpc::apis::system::SystemInfo {
 			chain_name: config.chain_spec.name().into(),
@@ -320,9 +321,8 @@ impl<Components: components::Components> Service<Components> {
 			client.clone(), network.clone(), has_bootnodes, system_info, config.rpc_http,
 			config.rpc_ws, config.rpc_cors.clone(), task_executor.clone(), transaction_pool.clone(),
 		)?;
-
+*/
 		let telemetry_connection_sinks: Arc<Mutex<Vec<mpsc::UnboundedSender<()>>>> = Default::default();
-
 		// Telemetry
 		let telemetry = config.telemetry_endpoints.clone().map(|endpoints| {
 			let is_authority = config.roles == Roles::AUTHORITY;
@@ -353,7 +353,6 @@ impl<Components: components::Components> Service<Components> {
 				}),
 			}))
 		});
-
 		Ok(Service {
 			client,
 			network: Some(network),
@@ -363,7 +362,7 @@ impl<Components: components::Components> Service<Components> {
 			keystore,
 			config,
 			exit,
-			_rpc: Box::new(rpc),
+			//_rpc: Box::new(rpc),
 			_telemetry: telemetry,
 			_offchain_workers: offchain_workers,
 			_telemetry_on_connect_sinks: telemetry_connection_sinks.clone(),
@@ -434,6 +433,7 @@ impl<Components> Drop for Service<Components> where Components: components::Comp
 	}
 }
 
+/*
 fn maybe_start_server<T, F>(address: Option<SocketAddr>, start: F) -> Result<Option<T>, io::Error>
 	where F: Fn(&SocketAddr) -> Result<T, io::Error>,
 {
@@ -450,7 +450,7 @@ fn maybe_start_server<T, F>(address: Option<SocketAddr>, start: F) -> Result<Opt
 			})?),
 		None => None,
 	})
-}
+}*/
 
 /// Transaction pool adapter.
 pub struct TransactionPoolAdapter<C: Components> {
