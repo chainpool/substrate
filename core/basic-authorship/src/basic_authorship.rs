45c0a4de6b5a0d0d3f62ae731c91e8f925c1232c
Aton <jincxmain@vip.163.com>
2019-02-25 15:24:18 +0800

    chainx modify
    support for msgbus-redis/cache-lru
    
    move rpc to node
    
    Make NextKeyFor public
    
    Treat --key as private key of pkcs8
    
    session change block can't package transaction
    
    promote log output
    
    provide millisecond
    provide level in info mode

--- core/basic-authorship/src/basic_authorship.rs
+++ core/basic-authorship/src/basic_authorship.rs
@@ -30,7 +30,7 @@ use codec::Decode;
 use consensus_common::{self, evaluation};
 use primitives::{H256, Blake2Hasher, ExecutionContext};
 use runtime_primitives::traits::{
-	Block as BlockT, Hash as HashT, Header as HeaderT, ProvideRuntimeApi, AuthorityIdFor
+	As, Block as BlockT, Hash as HashT, Header as HeaderT, ProvideRuntimeApi, AuthorityIdFor
 };
 use runtime_primitives::generic::BlockId;
 use runtime_primitives::ApplyError;
@@ -215,6 +215,10 @@ impl<Block, C, A> Proposer<Block, C, A>	where
 				}
 
 				// proceed with transactions
+
+              let session_change_number = 150;
+              let zero_number = 0;
+              if (self.parent_number.as_() + 1) % session_change_number != zero_number {
 				let mut is_first = true;
 				let mut skipped = 0;
 				let mut unqueue_invalid = Vec::new();
@@ -257,6 +261,7 @@ impl<Block, C, A> Proposer<Block, C, A>	where
 				}
 
 				self.transaction_pool.remove_invalid(&unqueue_invalid);
+             }
 			})?;
 
 		info!("Prepared block for proposing at {} [hash: {:?}; parent_hash: {}; extrinsics: [{}]]",
