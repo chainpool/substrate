2483fffd001e0c2d3c2d82b2793b91675a141e1b
gguoss <guanghuaguo.oss@gmail.com>
2019-06-17 12:29:49 +0800

    Add get_finalized_info in client

--- core/client/src/client.rs
+++ core/client/src/client.rs
@@ -72,7 +72,7 @@ use crate::error;
 use crate::in_mem;
 use crate::block_builder::{self, api::BlockBuilder as BlockBuilderAPI};
 use crate::genesis;
-use substrate_telemetry::{telemetry, SUBSTRATE_INFO, CONSENSUS_INFO};
+use substrate_telemetry::{telemetry, SUBSTRATE_INFO};
 
 use log::{info, trace, warn};
 
@@ -799,6 +799,11 @@ impl<B, E, Block, RA> Client<B, E, Block, RA> where
 			"best" => ?hash,
 			"origin" => ?origin
 		);
+
+		result
+	}
+
+    pub fn get_finalized_info(&self, hash: Block::Hash) -> (NumberFor<Block>, Option<Block::Hash>) {
 		let finalized_number = match self.storage(&BlockId::Hash(hash),
 				&StorageKey(well_known_keys::AURA_FINALIZE.to_vec())) {
 			Ok(Some(number)) => Decode::decode(&mut number.0.as_slice()).unwrap_or(NumberFor::<Block>::zero()),
@@ -806,14 +811,7 @@ impl<B, E, Block, RA> Client<B, E, Block, RA> where
 		};
 		let finalized_hash = self.block_hash(finalized_number).unwrap_or(None);
 
-		if !finalized_hash.is_none() {
-			telemetry!(CONSENSUS_INFO; "afg.finalized";
-				"finalized_number" => ?finalized_number,
-				"finalized_hash" => ?finalized_hash.unwrap(),
-			);
-		}
-
-		result
+		(finalized_number, finalized_hash)
 	}
 
 	fn execute_and_import_block(
